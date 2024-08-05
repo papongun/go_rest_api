7eb63e8560ab45052d6a32129fc94754fd3d1d76
papongun <papon.gun2@gmail.com>
2024-08-05 10:08:52 +0700

    refac: rm wrong format

--- entity/user_entity.go
+++ entity/user_entity.go
@@ -5,6 +5,6 @@ import "gorm.io/gorm"
 type User struct {
 	gorm.Model
 	Username    string `gorm:"uniqueIndex; not null"`
-	DisplayName string `gorm:"not null; "`
+	DisplayName string `gorm:"not null"`
 	Password    string `gorm:"not null"`
 }
